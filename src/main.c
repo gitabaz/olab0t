694859afd6e6f0afaa20fb9d95543ec14e250ca7
Aidan <olabaz@gmail.com>
2020-08-21 12:18:42 -0400

    A bit of cleaning up and refactoring in the main function.

--- src/main.c
+++ src/main.c
@@ -28,14 +28,17 @@
 /* :olabaz!olabaz@olabaz.tmi.twitch.tv PRIVMSG #olab0t :hello */
 /* :olabaz!olabaz@olabaz.tmi.twitch.tv PRIVMSG #olab0t :hi : */
 
+#define TWITCH_IRC_URL "irc.chat.twitch.tv"
+#define TWITCH_IRC_PORT "6667"
+
+void authenticate(bot_config* bc, int sock_fd);
+void join_channels(bot_config* bc, int sock_fd);
 
 int main() {
 
     puts("Started olab0t...");
 
     char* bot_config_fn = "bin/twitch_oauth.json";
-    const char* twitch_irc_url = "irc.chat.twitch.tv";
-    const char* twitch_irc_port = "6667";
 
     bot_config* bc = bot_config_from_file(bot_config_fn);
 
@@ -47,7 +50,7 @@ int main() {
     hints.ai_family = AF_UNSPEC; // Either IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
 
-    if ((status = getaddrinfo(twitch_irc_url, twitch_irc_port, &hints, &serv_info)) != 0) {
+    if ((status = getaddrinfo(TWITCH_IRC_URL, TWITCH_IRC_PORT, &hints, &serv_info)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(EXIT_FAILURE);
     }
@@ -57,58 +60,21 @@ int main() {
         puts("Error: could not get socket descriptor");
         exit(EXIT_FAILURE);
     }
+
     int sock_conn = connect(sock_fd, serv_info->ai_addr, serv_info->ai_addrlen);
     if (sock_conn != 0) {
         puts("Error: could not connect to socket descriptor");
         exit(EXIT_FAILURE);
     }
 
-    char pass[107], nick[107], channel[107]; // 101 char strings + prefix (`PASS `, `NICK `, `JOIN #`)
-    int bytes_sent, bytes_recv;
-    char buf[501];
-
-    // Authenticate
-    sprintf(pass, "PASS %s\n", bc->oauth_token);
-    sprintf(nick, "NICK %s\n", bc->bot_username);
-    bytes_sent = send(sock_fd, pass, strlen(pass), 0);
-    sleep(0.1);
-    bytes_sent = send(sock_fd, nick, strlen(nick), 0);
-
-    if (bytes_sent != strlen(nick)) {
-        exit(EXIT_FAILURE);
-    }
-
-    bytes_recv = recv(sock_fd, buf, 500, 0);
-    if (bytes_recv == 0) {
-        puts("Error: could not connect");
-        exit(EXIT_FAILURE);
-    }
-    fwrite(buf, 1, bytes_recv, stdout);
-
     // TODO: make sure we are actually authenticated
+    authenticate(bc, sock_fd);
 
-    // Join channels in list
-    if (bc->capacity == 0) {
-        puts("Error: no channels added");
-        exit(EXIT_FAILURE);
-    }
+    join_channels(bc, sock_fd);
 
-    for (int i = 0; i < bc->capacity; i++) {
-        sprintf(channel, "JOIN #%s\n", bc->channel_list[i]);
-        bytes_sent = send(sock_fd, channel, strlen(channel), 0);
-
-        if (bytes_sent != strlen(channel)) {
-            printf("Error: could not connect to channel `%s`\n", bc->channel_list[i]);
-        }
-        bytes_recv = recv(sock_fd, buf, 500, 0);
-        sleep(0.1);
-        if (bytes_recv == 0) {
-            printf("Error: could not connect to channel `%s`\n", bc->channel_list[i]);
-        }
-        fwrite(buf, 1, bytes_recv, stdout);
-    }
-
+    char buf[501];
     char ping[6], msg[301], msg_channel[101], msg_user[101];
+    ssize_t bytes_recv;
     while(true) {
         bytes_recv = recv(sock_fd, buf, 500, 0);
         if (bytes_recv > 0) {
@@ -123,4 +89,71 @@ int main() {
     freebc(bc);
 
     return 0;
+}
+
+void authenticate(bot_config* bc, int sock_fd) {
+    char pass[110], nick[110]; // 100 char strings + extra space to store prefix (`PASS `, `NICK `)
+    ssize_t bytes_sent, bytes_recv;
+    char buf[501];
+
+    // In parse config, should we just read in the strings into the proper
+    // format so we don't have to sprintf them here?
+    sprintf(pass, "PASS %s\n", bc->oauth_token);
+    sprintf(nick, "NICK %s\n", bc->bot_username);
+
+    bytes_sent = send(sock_fd, pass, strlen(pass), 0);
+    if (bytes_sent < 0) {
+        exit(EXIT_FAILURE);
+    } else {
+        if ((size_t) bytes_sent != strlen(pass)) {
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    bytes_sent = send(sock_fd, nick, strlen(nick), 0);
+    if (bytes_sent < 0) {
+        exit(EXIT_FAILURE);
+    } else {
+        if ((size_t) bytes_sent != strlen(nick)) {
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    bytes_recv = recv(sock_fd, buf, 500, 0);
+    if (bytes_recv <= 0) {
+        puts("Error: could not connect");
+        exit(EXIT_FAILURE);
+    }
+    fwrite(buf, 1, bytes_recv, stdout);
+}
+
+void join_channels(bot_config* bc, int sock_fd) {
+    char channel[107]; // 100 char string + extra space to store prefix (`JOIN #`)
+    char buf[501];
+    ssize_t bytes_sent, bytes_recv;
+
+    if (bc->capacity == 0) {
+        puts("Error: no channels added");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < bc->capacity; i++) {
+        sprintf(channel, "JOIN #%s\n", bc->channel_list[i]);
+        bytes_sent = send(sock_fd, channel, strlen(channel), 0);
+
+        if(bytes_sent < 0){
+            exit(EXIT_FAILURE);
+        } else {
+            if ((size_t) bytes_sent != strlen(channel)) {
+                printf("Error: could not connect to channel `%s`\n", bc->channel_list[i]);
+            }
+        }
+
+        bytes_recv = recv(sock_fd, buf, 500, 0);
+        if (bytes_recv <= 0) {
+            printf("Error: could not connect to channel `%s`\n", bc->channel_list[i]);
+        }
+        fwrite(buf, 1, bytes_recv, stdout);
+    }
+
 }
